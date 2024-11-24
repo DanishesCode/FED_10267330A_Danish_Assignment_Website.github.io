8a7c53a749077720f527865e804d6d2ff55ed23e
DanishesCode <ezogetsthis@gmail.com>
2024-11-24 22:05:55 +0800

    added js files for all files

---
+++ contact.js
@@ -0,0 +1,19 @@
+/*navigation functions*/
+function navigateMenu(){
+    window.location.href = "menu.html"; 
+}
+function navigateLocation(){
+    window.location.href = "location.html"; 
+}
+function navigateContact(){
+    window.location.href = "Contact.html"; 
+}
+function navigateTracking(){
+    window.location.href = "tracking.html"; 
+}
+function navigateCheckout(){
+    window.location.href = "checkout.html"; 
+}
+function navigateIndex(){
+    window.location.href = "index.html"; 
+}
