a5bea4dd8c0efaba9be3a721a519f56219cbe5ee
DanishesCode <ezogetsthis@gmail.com>
2024-11-24 17:14:14 +0800

    added datasets and doing js for filtering

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const filterButtons = document.querySelectorAll('.filterContainer button');
-const allButton = document.getElementById("all");
-console.log(allButton);
-filterButtons.forEach = function(){
-    console.log('h');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    searchFilter();
+    filterFunc();
+});
 
 
+
+/*values*/
+let filter = "all";
 /*navigation functions*/
 function navigateMenu(){
     window.location.href = "menu.html"; 
@@ -24,4 +25,50 @@ function navigateCheckout(){
 }
 function navigateIndex(){
     window.location.href = "index.html"; 
+}
+function searchFilter(){
+    const searchInputElement = document.querySelector(".search-bar");
+    const menuItemElements = document.querySelectorAll(".menu-item");
+    searchInputElement.addEventListener("input", function () {
+        const searchQueryText = searchInputElement.value.toLowerCase();
+        let selected;
+        menuItemElements.forEach(function (menuItemElement) {
+            const itemNameText = menuItemElement.querySelector(".item-name").textContent.toLowerCase();
+            if (itemNameText.includes(searchQueryText)) {
+                menuItemElement.style.display = "block";
+                selected = menuItemElement;
+            } else {
+                menuItemElement.style.display = "none"; 
+            }
+            
+            filter = selected.dataset.category;
+        });
+    });
+}
+function filterFunc(){
+    const filterButtons = document.querySelectorAll(".filterContainer button");
+    filterButtons.forEach(function(x){
+        if(x.getAttribute(id) == filter){
+            x.style.color = "red";
+        }else{
+            x.style.color = "white";
+        }
+    })
+
+
+    filterButtons.forEach(function(pressed){
+        pressed.addEventListener("click",function(){
+            const cat = pressed.getAttribute("id");
+            filter = cat;
+            filterButtons.forEach(function(x){
+                if(x == pressed){
+                    x.style.color = "red";
+                }else{
+                    x.style.color = "white";
+                }
+            })
+            
+        })
+    })
+
 }
