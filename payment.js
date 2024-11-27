ae25dc655a2d1e0b136b6e319a51a9c2fdf46a45
DanishesCode <ezogetsthis@gmail.com>
2024-11-27 20:52:42 +0800

    added animation on pay

--- payment.js
+++ payment.js
@@ -18,18 +18,31 @@ function getRandomTracking(){
     console.log(list);
     return trackingId;
 }
+
 function pay(){
+    let container = document.getElementById("lottie");
+    var animation = lottie.loadAnimation({
+        container: container,  
+        renderer: 'svg', 
+        loop: false, 
+        autoplay: false, 
+        path: 'animation/tick.json'  
+    });
     const payButton = document.querySelector(".payment-form button");
     const form = document.querySelector("form");
     amount = getAmount();
     payButton.textContent = "Pay $"+amount;
-    form.addEventListener("submit",function(){
+    form.addEventListener("submit",function(x){
+        x.preventDefault();
+        container.style.display = "block";
+        animation.play();
+        container.style.display = "block";
+        animation.addEventListener('complete', function () {
+        container.style.display = "none";
         localStorage.removeItem("cart");
         window.location.href = "success.html"; 
         let id = getRandomTracking();
-        localStorage.setItem("currentTracking",id)
-        alert("Successful payment");
+        localStorage.setItem("currentTracking",id);
+        });
     })
-    
-    
 }
